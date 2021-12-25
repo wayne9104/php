765746aa79420dc16a9a24ef970af8804fcaa2f9
wayne9104 <82804534+wayne9104@users.noreply.github.com>
2021-12-25 15:15:52 +0800

    Add files via upload

---
+++ 22.bulletin_add_form.php
@@ -0,0 +1,14 @@
+<html>
+    <head><title>新增布告</title></head>
+    <body>
+        <form method=post action=bulletin_add.php>
+            標  題:<input type=text name=title><br>
+            內  容:<br><textarea name=content rows=20 cols=20></textarea><br>
+            佈告類型:<input type=radio name=type value=1>系上公告
+                    <input type=radio name=type value=2>獲獎資訊
+                    <input type=radio name=type value=3>徵才資訊<br>
+            發布時間:<input type=date name=time><p></p>
+            <input type=submit value=新增佈告> <input type=reset value=清除>
+        </form>
+    </body>
+</html>
